380b43336cbeefe384f0dfd7cce9b5a9a0c19dd2
vu hai dang <haidangdevhaui@gmail.com>
2018-01-16 12:53:29 +0700

    add font rule

--- config/webpack.js
+++ config/webpack.js
@@ -43,6 +43,9 @@ module.exports = {
                     }
                 }]
             })
+        }, {
+            test: /\.(gif|jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+            loader: 'url-loader?limit=100000'
         }]
     },
     plugins: plugins
